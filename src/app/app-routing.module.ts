b84c82e8ed8f90d0ebe1082f3f19233ad19d0ae6
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-04-04 20:23:01 -0500

    chore: adding main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoreComponent } from './components/core/core.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'index', component: CoreComponent },
+  { path: '', redirectTo: '/index', pathMatch: 'full' }, // redirect to `first-component`];
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
