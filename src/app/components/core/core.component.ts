fb491de65948740eeb15f019ec52ffe16f0acfaa
Evert Ortiz <evert.ortiz@rabbitmx.com>
2024-04-05 11:09:46 -0500

    refactor: adding service to return paragraph

--- src/app/components/core/core.component.ts
+++ src/app/components/core/core.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TextService } from '../../services/text-service/text.service';
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
-  styleUrls: ['./core.component.css']
+  styleUrls: ['./core.component.scss'],
 })
 export class CoreComponent implements OnInit {
+  paragraph: string = '';
+  constructor(private textService: TextService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    const path = '../../../assets/text.txt'; // Aquí debes proporcionar la ruta correcta al archivo de texto
+    const wordCount = 20; // Número de palabras que deseas en el párrafo
+    this.textService.getParagraph(path, wordCount);
+    this.textService.text$.subscribe((text: string) => {
+      this.paragraph = text;
+    });
   }
-
 }
---
+++ src/app/services/text-service/text.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TextService {
+  private textSubject: BehaviorSubject<string> = new BehaviorSubject<string>(
+    ''
+  );
+  public text$: Observable<string> = this.textSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  getParagraph(path: string, wordCount: number): void {
+    this.http
+      .get(path, { responseType: 'text' })
+      .pipe(map((text: string) => this.extractParagraph(text, wordCount)))
+      .subscribe((paragraph: string) => {
+        this.textSubject.next(paragraph);
+      });
+  }
+
+  private extractParagraph(text: string, wordCount: number): string {
+    const words = text.split(/\s+/); // Separar el texto en palabras
+    let paragraph = '';
+    if (words.length > wordCount) {
+      const startIndex = this.generateRandomIndex(words.length - wordCount); // Índice de inicio aleatorio
+      paragraph = words.slice(startIndex, startIndex + wordCount).join(' '); // Extraer N palabras continuas
+    } else {
+      paragraph = words.join(' '); // Si el texto tiene menos palabras que la cantidad deseada, devolver el texto completo
+    }
+    return paragraph;
+  }
+
+  private generateRandomIndex(max: number): number {
+    return Math.floor(Math.random() * (max + 1));
+  }
+}
